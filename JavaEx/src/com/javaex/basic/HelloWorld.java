e46b45b0fa6f91cab55fce266f525be9583f3b65
Kim Se Yun <ksy642@naver.com>
2020-08-04 11:05:20 +0900

    첫 번째 커밋

---
+++ JavaEx/src/com/javaex/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.javaex.basic;
+
+/*
+ * 화면에 Hello, World라 출력 해주자
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, Wolrd!");
+	}
+
+}
+ 
