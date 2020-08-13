f3a03e69e81ff02e349bc638654d2f09900311f7
ksy642 <ksy642@naver.com>
2020-08-13 10:49:44 +0900

    8월 13일 목요일 생성자, 오버로딩 연습

---
+++ JavaEx/src/com/javaex/oop/goods/v3/GoodApp.java
@@ -0,0 +1,46 @@
+package com.javaex.oop.goods.v3;
+
+// v3.
+// point v1을 복사해서 v2로...
+// Goods 객체를 생성하여 사용하는 클래스
+public class GoodApp {
+
+	public static void main(String[] args) {
+		// Goods 클래스 사용
+//		Goods notebook = new Goods(); // 인스턴스화 -> 생성자가 없어서 주석처리
+		// 필드에 접근
+		
+		Goods notebook = new Goods("LG Gram", 150000);
+		//생성자를 통해 초기화가 된다
+		
+		/*
+		notebook.name = "LG Gram";
+		notebook.price = 1500000;
+		
+		System.out.printf("%s, %d원%n",
+				notebook.name, notebook.price);
+		*/
+		
+		// setter를 이용한 우회 접근
+		notebook.setName("LG Gram");
+		notebook.setPrice(1500000);
+		// 메서드 호출
+		notebook.showInfo();
+		
+//		Goods smartphone = new Goods(); // 생성자가 없어서 주석처리
+		
+		Goods smartphone = new Goods("iPhone SE", 600000);
+		
+	/*
+		smartphone.setName("Phone SE");
+		smartphone.setPrice(600000);
+	*/	smartphone.showInfo();
+	
+	}
+
+}
+
+
+
+
+
---
+++ JavaEx/src/com/javaex/oop/point/v2/PointApp.java
@@ -0,0 +1,30 @@
+package com.javaex.oop.point.v2;
+
+// v2
+public class PointApp {
+	// 필드
+	private int x;
+	private int y;
+	
+	// getters/setters
+	public int getX() {	return x; }
+	public void setX(int x) { this.x = x; }
+	public int getY() {	return y; }
+	public void setY(int y) { this.y = y; }
+	// 메서드
+	public void draw() { System.out.printf("점(%d, %d)을 그렸습니다.%n", x, y); }
+	
+	public static void main(String[] args) {
+		Point p1 = new Point(); // 인스턴스화
+		p1.setX(10);
+		p1.setY(20);
+		
+		Point p2 = new Point(); // 인스턴스화
+//		p2.setX(20);
+//		p2.setX(30);
+		
+		p1.draw();
+		p2.draw();
+	}
+
+}
